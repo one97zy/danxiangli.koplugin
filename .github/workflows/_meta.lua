be8e508e2166cfa2ac21b896617839f2796c6a40
one97zy <50978578+one97zy@users.noreply.github.com>
2026-09-21 10:18:12 +0800

    Delete .github directory

--- .github/workflows/_meta.lua
+++
@@ -1,9 +0,0 @@
-local _ = require("gettext")
-return {
-    fullname = _("单向历下载"),
-    description = _("开机自动下载当日单向历，支持手动触发，自动清理过期图片；兼容Kindle墨水屏设备"),
-    author = "Your Name",
-    version = "1.2",
-    depends = {},
-    device_support = {"Kindle"},
-}
